a05b8a72fe8bbb79ac76dca0c2f947982606cb07
clyde <clydesavvy@gmail.com>
2021-10-01 19:25:00 +0800

    Routes Api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/users', [VueController::class, 'index']);
+Route::post('/user', [VueController::class, 'store']);
+Route::get('/user/{id}', [VueController::class, 'show']);
+Route::put('/user/{id}/update', [VueController::class, 'edit']);
+Route::delete('/user/{id}/delete', [VueController::class, 'destroy']);
