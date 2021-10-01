866a95e9fc48d9922199e7f68c6f3392c9aac0f5
clyde <clydesavvy@gmail.com>
2021-10-01 19:19:58 +0800

    VueController

--- app/Http/Controllers/VueController.php
+++ app/Http/Controllers/VueController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class VueController extends Controller
 {
@@ -14,17 +15,8 @@ class VueController extends Controller
      */
     public function index()
     {
-        return User::orderBy('last_name')->all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $users = User::all()->toArray();
+        return array_reverse($users);
     }
 
     /**
@@ -35,7 +27,24 @@ class VueController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'first' => 'required',
+            'last' => 'required',
+            'username' => 'required|unique:user',
+            'password' => 'required|confirmed',
+            'role' => 'required'
+        ]);
+
+        $create = User::create([
+            'first_name' => ucwords($request->first),
+            'last_name' => ucwords($request->last),
+            'username' => $request->username,
+            'password' => Hash::make($request->passwprd),
+            'role' => $request->role
+        ]);
+
+        return response()
+                ->json(['message' => 'The user was added successfully.', 'data' => $create], 200);
     }
 
     /**
@@ -46,7 +55,11 @@ class VueController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) {
+            return response()->json('User not found!', 404);
+        }
+        return response()->json($user, 200);
     }
 
     /**
@@ -55,21 +68,29 @@ class VueController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
-        //
-    }
+        $user = User::find($id);
+        if (is_null($user)) {
+            return response()->json('User not found!', 404);
+        }
+        $user->first_name = ucwords($request->first);
+        $user->last_name = ucwords($request->last);
+        $user->username = $request->username;
+        if ($request->filled('password')) {
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+            if ($request->password != $request->confirm) {
+                return response()->json('Password does not match.', 200);
+            }
+
+            $user->password = Hash::make($request->password);
+
+        }
+        $user->role = $request->role;
+        $user->save();
+
+        return response()
+                ->json(['message' => 'The user was updated successfully.', 'data' => $user], 200);
     }
 
     /**
@@ -80,6 +101,12 @@ class VueController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        if (is_null($user)) {
+            return response()->json('User not found!', 404);
+        }
+        $user->delete();
+
+        return response()->json('The user deleted successfully', 200);
     }
 }
